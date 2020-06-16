15a2af340451e1b45adb250ef95e59f13ccf01a4
Jordan Crane <jordan@jordancrane.me>
2020-06-16 15:53:02 -0700

    Modify build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker-compose up -d
-mkdir -p public
+mkdir public
 cd output
-\cp -f resume.html ../public/index.html
-\cp -f resume.pdf resume.rtf resume.docx ../public/
+cp resume.html ../public/index.html
+cp resume.pdf resume.rtf resume.docx ../public/
